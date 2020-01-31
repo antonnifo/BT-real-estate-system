d85e2dda5d2608d651402337e74ec455debcb748
antonnifo <antonnifo@live.com>
2020-01-31 16:07:57 +0300

    add realtors model

--- src/realtors/models.py
+++ src/realtors/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Realtor(models.Model):
+    name        = models.CharField(max_length=100)
+    photo       = models.ImageField(upload_to='realtors/%Y/%m/%d')
+    description = models.TextField(max_length=200, blank=True)
+    email       = models.TextField(max_length=200, unique=True)
+    phone       = models.IntegerField(unique=True)
+    is_mvp      = models.BooleanField(default=False)
+    hire_date   = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name        = 'realtor'
+        verbose_name_plural = 'realtors'          
