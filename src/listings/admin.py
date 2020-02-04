d5c15d33cb1233227b3f4fc603a9c73b54a5c0b1
antonnifo <antonnifo@live.com>
2020-02-04 13:11:17 +0300

    add more visible listing fields in admin site

--- src/listings/admin.py
+++ src/listings/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display       = ('id', 'title', 'is_punlished','price','list_date', 'realtor')
+    list_display_links =('id', 'title')
+    list_filter        = ('realtor', )
+    list_editable      = ('is_punlished', )
+    search_fields      = ('title', 'description', 'address', 'city','county', 'price')
+    list_per_page      = 25
+
+admin.site.register(Listing, ListingAdmin)
