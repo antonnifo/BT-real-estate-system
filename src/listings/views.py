dfbf1e1066259c8ae8f0250bca1155a0fcc7b555
antonnifo <antonnifo@live.com>
2020-02-06 21:27:08 +0300

    order and filter listings

--- src/listings/views.py
+++ src/listings/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from .models import Listing
+
+def listings(request):
+    template = 'listings/listings.html'
+
+    listings = Listing.objects.order_by('-list_date').filter(is_punlished=True)
+
+    paginator = Paginator(listings,6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context  = {
+               'listings':paged_listings,
+    }
+    return render(request, template,context)
+
+def listing(request, id):
+
+    template = 'listings/listing.html'
+    return render(request, template)
+
+
+def search(request):
+
+    template = 'listings/search.html'
+    return render(request, template)
+   
