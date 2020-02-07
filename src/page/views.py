c2d4948316536f707ec4f0e2d21a20582bdba406
antonnifo <antonnifo@live.com>
2020-02-07 20:55:41 +0300

    use realtors and listing models in the respective functions

--- src/page/views.py
+++ src/page/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 
-# Create your views here.
+def index(request):
+
+    template = 'page/index.html'
+
+    listings = Listing.objects.order_by('-list_date').filter(is_punlished=True)[:3]
+
+    context = {
+           "listings": listings,
+    }
+    return render(request, template, context)
+
+def about(request):
+    
+    template = 'page/about.html'
+
+    realtor = Realtor.objects.order_by('-hire_date')
+
+    mvp     = Realtor.objects.all().filter(is_mvp=True)
+    
+    context = {
+        "realtors" : realtor,
+        "mvp"      :mvp,
+    }
+    return render(request, template, context)
